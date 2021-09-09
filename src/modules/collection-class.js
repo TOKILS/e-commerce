e88418393d06290943e0a1e5bf7615c385b35c11
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-09-09 14:50:15 +0300

    update data-collection

--- src/modules/collection-class.js
+++ src/modules/collection-class.js
@@ -6,7 +6,7 @@ class DataCollection {
 
   get(id) {
       if (id) { 
-          return this.model.findOne({ id });
+          return this.model.findOne({ where:{ id: id}});
       }
       else {
           return this.model.findAll({});
