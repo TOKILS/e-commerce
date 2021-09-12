25f6b6f2d1adfd55a67c296acd20d6c6b24a1c4b
KZTahat <kztahat96@gmail.com>
2021-09-12 14:59:40 +0300

    pulling changes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 // 3rd Party Resources
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // Esoteric Resources
-const notFoundHandler = require('./error-handlers/404');
-const errorHandler = require('./error-handlers/500');
-const loggerMiddleware = require('./middleware/logger');
+const notFoundHandler = require("./error-handlers/404");
+const errorHandler = require("./error-handlers/500");
+const loggerMiddleware = require("./middleware/logger");
 
-const authRoutes = require('./routes/authRoutes.js');
-const v2Routes = require('./routes/v2.js');
+const authRoutes = require("./routes/authRoutes.js");
+const v2Routes = require("./routes/v2.js");
 
 // Prepare the express app
 const app = express();
@@ -21,23 +21,23 @@ app.use(express.json());
 
 app.use(loggerMiddleware);
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello 👋 🖥 server')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello 👋 🖥 server");
+});
 
 app.use(authRoutes);
-app.use('/api/v2', v2Routes);
+app.use("/api/v2", v2Routes);
 
 // Catchalls
-app.use('*', notFoundHandler);
+app.use("*", notFoundHandler);
 app.use(errorHandler);
 
-const start = PORT => {
-    app.listen(PORT, () => {
-        console.log(`Server is Running on Port ${PORT}`)
-    })
-}
+const start = (PORT) => {
+  app.listen(PORT, () => {
+    console.log(`Server is Running on Port ${PORT}`);
+  });
+};
 module.exports = {
-    server: app,
-    start: start
-}
+  server: app,
+  start: start,
+};
