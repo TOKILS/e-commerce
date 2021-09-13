36e6f8b36a5ba5ee2f841f42e509db25fd55285a
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-09-13 22:35:41 +0300

    client 99% done

--- __tests__socket/client/client.js
+++ __tests__socket/client/client.js
@@ -1,48 +1,81 @@
-// 'use strict';
+"use strict";
+console.log("client.js is running");
 
+// ? consts and variables
+const form = document.getElementById("form");
 const messageInput = document.getElementById("message-input");
 
-const form = document.getElementById("form");
+const form2 = document.getElementById("form2");
+const roomInput = document.getElementById("room-input");
 
-console.log("client.js file ran");
 const socket = io("http://localhost:3050");
 
-socket.on("connect", () => {
-  displayMessage(`you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}`, "myMessages")
+let email = "suhaibersan@gmail.com";
 
-  displayMessage(`you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}`, "forMeMessages")
-  displayMessage(`you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}`, "forMeMessages")
-  displayMessage(`you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}`, "forMeMessages")
-  displayMessage(`you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}you connected with id: ${socket.id}`, "forMeMessages")
-})
-
-
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   let message = messageInput.value;
   console.log();
-  
+
   if (message === "") return;
 
   displayMessage(message, "myMessages");
-
+  sendMessageToIo(message, "suhaibersan@gmail.com");
   messageInput.value = "";
 });
 
+form2.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let room = roomInput.value;
 
+  socket.emit("join-specific-room", room)
+});
 
-function displayMessage(message, classType = "forMeMessages") {
+// ? socket code portion
+// ! things to do when connected
+socket.on("connect", async () => {
+  await socket.emit("join-room", { email: email });
+});
+
+// ! socket.io send and receive
+function sendMessageToIo(message) {
+  socket.emit("send-message", { message });
+}
+socket.on("receive-message", (payload) => {
+  /* payload = {
+      message: "message here",
+      email: "email here",
+      roomID: "uuid here",
+      messageType: "forMeMessages"
+    } */
+  displayMessage(payload.message, payload.messageType);
+});
+
+socket.on("info", (payload) => {
+  displayMessage(payload.message, payload.messageType);
+});
+
+socket.on("receive-old-messages", (payload) => {
+  for (let i = 0; i < payload.messages.length; i++) {
+    displayMessage(payload.messages[i], 'forMeMessages');
+  }  
+});
+
+// ! function to write messages to the HTML
+function displayMessage(message, classType = "systemMessages") {
   let divContainer = document.createElement("div");
   if (classType === "myMessages") {
     divContainer.setAttribute("class", "myMessagesContainer");
+  } else if (classType === "systemMessages") {
+    divContainer.setAttribute("class", "systemMessagesContainer");
   } else {
     divContainer.setAttribute("class", "forMeMessagesContainer");
   }
 
   let div = document.createElement("div");
-  div.setAttribute("class", classType)
+  div.setAttribute("class", classType);
   div.textContent = message;
-  divContainer.append(div)
+  divContainer.append(div);
   let first = document.getElementById("message-container");
   if (first.firstChild) {
     document.getElementById("message-container").insertBefore(divContainer, first.firstChild);
--- __tests__socket/server/server.js
+++ __tests__socket/server/server.js
@@ -18,21 +18,23 @@ messageQueue = {
 */
 
 io.on("connection", (socket) => {
-  console.log(`${socket.id} connected to the server `, socket.remoteAddress);
+  console.log(`${socket.id} connected to the server`);
   /* when a user connects, make a room and add it with the email of the user to the object userRooms */
-  socket.on("help", (payload) => {
-    console.log("client need help", payload.email);
 
+  socket.on("join-room", (payload) => {
     let newRoomID = uuid();
     socket.join(newRoomID);
+
+    socket.infoObj = {
+      email: payload.email,
+      room: newRoomID,
+    };
 
     // check if the user has any messages in the message queue
     if (messageQueue[payload.email]) {
       messageQueue[payload.email].room = newRoomID;
-      // front end will have a io.on(user email) event           "receive-room-data" io event
-      io.emit(payload.email, {
-        data: messageQueue[payload.email],
-      });
+      let messageFromQ = messageQueue[payload.email].messages;
+      socket.emit("receive-old-messages", { messages: messageFromQ });
     } else {
       messageQueue[payload.email] = {
         room: newRoomID,
@@ -40,22 +42,71 @@ io.on("connection", (socket) => {
       };
     }
 
-    /* when a front end wants to send a message it will emit to the message-send event */
-    // socket.on("message-send", async (payload) => {
-    socket.on(payload.email, async (userMessage) => {
-      /* payload = {
-        message: "message here",
-        email: "email here",
-        roomID: "uuid here",
-      } */
+    socket.emit("info", { message: `you are now connected to room ${newRoomID}`, messageType: "systemMessages" });
+  });
 
-      io.to(userMessage.roomID).emit(payload.email, userMessage.message);
-      /* this event is to send the previous message to the room they're in */
+  socket.on("join-specific-room", room => {
+    socket.join(room);
+    socket.infoObj.room = room;
+    socket.emit("info", { message: `you are now connected to room ${room}`, messageType: "systemMessages" });
+  })
 
-      // save message to database
-      // await messageModel.create(userMessage);
-      // save message to message queue
-      messageQueue[payload.email].message.push(userMessage);
-    });
+  /* when a front end wants to send a message it will emit to the message-send event */
+  socket.on("send-message", async (payload) => {
+    /* payload = {
+      message: "message here",
+    } */
+    socket.to(socket.infoObj.room).emit("receive-message", {message: payload.message, messageType: "forMeMessages"});
+    /* this event is to send the previous message to the room they're in */
+
+    // save message to database
+    // await messageModel.create(payload);
+    // save message to message queue
+    messageQueue[socket.infoObj.email].messages.push(payload.message);
   });
 });
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+// io.on("connection", (socket) => {
+//   console.log(`${socket.id} connected to the server `, socket.remoteAddress);
+//   /* when a user connects, make a room and add it with the email of the user to the object userRooms */
+//   socket.on("help", (payload) => {
+//     console.log("client need help", payload.email);
+
+//     let newRoomID = uuid();
+//     socket.join(newRoomID);
+
+//     // check if the user has any messages in the message queue
+//     if (messageQueue[payload.email]) {
+//       messageQueue[payload.email].room = newRoomID;
+//       // front end will have a io.on(user email) event           "receive-room-data" io event
+//       io.emit(payload.email, {
+//         data: messageQueue[payload.email],
+//       });
+//     } else {
+//       messageQueue[payload.email] = {
+//         room: newRoomID,
+//         messages: [],
+//       };
+//     }
+
+//     /* when a front end wants to send a message it will emit to the message-send event */
+//     // socket.on("message-send", async (payload) => {
+//     socket.on(payload.email, async (userMessage) => {
+//       /* payload = {
+//         message: "message here",
+//         email: "email here",
+//         roomID: "uuid here",
+//       } */
+
+//       io.to(userMessage.roomID).emit(payload.email, userMessage.message);
+//       /* this event is to send the previous message to the room they're in */
+
+//       // save message to database
+//       // await messageModel.create(userMessage);
+//       // save message to message queue
+//       messageQueue[payload.email].message.push(userMessage);
+//     });
+//   });
+// });
